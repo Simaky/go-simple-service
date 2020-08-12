016799de5648d7f4fe863477545f56c4bb1d47a2
Simaky <maxympritika@gmail.com>
2020-08-12 18:39:48 +0300

    Add sample handler test

--- src/go.mod
+++ src/go.mod
@@ -3,7 +3,6 @@ module go-rest-project
 go 1.14
 
 require (
-	github.com/fatih/structs v1.1.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/sessions v1.2.0
--- src/web/users_handler_test.go
+++ src/web/users_handler_test.go
@@ -1,30 +1,38 @@
 package web
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go-rest-project/db"
+	"go-rest-project/model"
 )
 
 func TestGetUsers(t *testing.T) {
-	req, err := http.NewRequest("GET", "/health-check", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	db.Mock()
+	expectedUser := model.User{ID: 1, Login: "test"}
 
-	rr := httptest.NewRecorder()
+	model.MockUsers(t, expectedUser)
+
+	req, err := http.NewRequest("GET", withVersion(users), nil)
+	assert.NoError(t, err)
+
+	r := httptest.NewRecorder()
 	handler := http.HandlerFunc(GetUsers)
 
-	handler.ServeHTTP(rr, req)
+	handler.ServeHTTP(r, req)
 
-	if status := rr.Code; status != http.StatusOK {
+	if status := r.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
 
-	expected := `{"alive": true}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
+	var user []model.User
+	err = json.NewDecoder(r.Body).Decode(&user)
+	assert.NoError(t, err)
+	assert.ObjectsAreEqual(expectedUser, user)
 }
